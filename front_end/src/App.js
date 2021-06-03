c5c0c56230a9e8c573645313fe7033ace742efaa
stellapark401 <stellapark401@gmail.com>
2021-06-03 17:35:06 +0900

    first commit

---
+++ front_end/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const App = () => {
+  return(
+    <>
+      <nav>
+        
+      </nav>
+
+      <h1>Biggest Headline</h1>
+      <p>When you are writing a paragraph. Use p_tag. Hence the name.</p>
+      
+      <hr/>
+      <Link to="http://www.google.com">Visit Google</Link>
+      <img src="./baby peach.jpg" width="600px"></img>
+    </>
+  )
+} 
+
+export default App
